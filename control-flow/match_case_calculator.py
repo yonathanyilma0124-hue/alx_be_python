e75125474eb15d9fe2637c69b4d67700f8426025
Yonathan Yilma <yonathanyilma0124@gmail.com>
2025-11-24 06:23:36 -0800

    Fix prompts and match-case logic

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
-num1 = float(input("Enter the first number?"))
-num2 = float(input("Enter the second number?"))
-operation = input("choose the operation (+, -, *, /)")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("choose the operation (+, -, *, /): ")
 match operation:
  case "+":
   print(f"The result is {num1 + num2} .")
