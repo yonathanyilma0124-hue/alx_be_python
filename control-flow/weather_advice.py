6fcb4550db59d8f32620761f6cd8e55bfe45a544
Yonathan Yilma <yonathanyilma0124@gmail.com>
2025-11-24 05:47:08 -0800

    Fix weather conditions by select the one weather and you will get suggestion for the weather

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
+if weather == "sunny":
+  print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+ print("don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+ print("Make sure to wear a warm coat and a scarf.")
+else:
+ print("sorry, I don't have recommendations for this weather.")
