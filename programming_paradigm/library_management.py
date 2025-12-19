6ab555784c98d367ea0b5583975f8ab59f1e1a73
Yonathan Yilma <yonathanyilma0124@gmail.com>
2025-12-18 23:02:30 -0800

    Add library_management.py to manage library books

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,50 @@
+class Book:
+    """Represents a book in the library."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # private attribute
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as available."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    """Manages a collection of books."""
+
+    def __init__(self):
+        self._books = []  # private list of books
+
+    def add_book(self, book):
+        """Add a book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return
+
+    def list_available_books(self):
+        """List all available books."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
